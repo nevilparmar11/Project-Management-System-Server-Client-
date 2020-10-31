87767c8ce3d3c635ba37ca2782c813a0d77282e5
nevilparmar11 <nevilparmar24@gmail.com>
2020-11-01 01:00:48 +0530

    logout functionality and sidebar links added

--- frontend/src/app/project/config/sidebar.ts
+++ frontend/src/app/project/config/sidebar.ts
@@ -3,9 +3,13 @@ import { SideBarLink } from '@nevilparmar11/interface/ui-model/nav-link';
 export const SideBarLinks = [
   new SideBarLink('Kanban Board', 'board', 'board'),
   new SideBarLink('Project Settings', 'cog', 'settings'),
-  new SideBarLink('Releases', 'ship'),
-  new SideBarLink('Issues and filters', 'filters'),
-  new SideBarLink('Pages', 'page'),
-  new SideBarLink('Reports', 'report'),
-  new SideBarLink('Components', 'component')
+  new SideBarLink('Repository','github',"https://github.com/nevilparmar11/project-management-system-server-client-"),
+  new SideBarLink('Feedback','feedback','https://google.com'),
+  new SideBarLink('My Profile','chevron-right','user-profile'),
+  // new SideBarLink('Logout', 'arrow-up','../account/logout'),
+  // new SideBarLink('Releases','ship'),
+  // new SideBarLink('Issues and filters', 'filters'),
+  // new SideBarLink('Pages', 'page'),
+  // new SideBarLink('Reports', 'report'),
+  // new SideBarLink('Components', 'component')
 ];
--- frontend/src/app/project/pages/board/board.component.ts
+++ frontend/src/app/project/pages/board/board.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GoogleAnalyticsService } from '@nevilparmar11/core/services/google-analytics.service';
+import {AccountService} from '@nevilparmar11/_services/account.service';
 
 @Component({
   selector: 'board',
@@ -9,9 +10,13 @@ import { GoogleAnalyticsService } from '@nevilparmar11/core/services/google-anal
 export class BoardComponent implements OnInit {
   breadcrumbs: string[] = ['Projects', 'Project Management System', 'Dashboard'];
 
-  constructor(private _googleAnalytics: GoogleAnalyticsService) {}
+  constructor(private _googleAnalytics: GoogleAnalyticsService, private accountService : AccountService) {}
 
   ngOnInit(): void {}
+
+  logout() {
+    this.accountService.logout();
+  }
 
   sendTwitterEventButton() {
     this._googleAnalytics.sendEvent('Share Twitter', 'button');
